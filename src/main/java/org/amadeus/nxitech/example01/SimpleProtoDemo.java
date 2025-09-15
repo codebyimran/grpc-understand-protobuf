614348ef8ade456058b85ad5b9a9afb37e9a817f
Imran <imran.md30@gmail.com>
2025-09-15 18:53:37 +0530

    Update SimpleProtoDemo.java

--- src/main/java/org/amadeus/nxitech/example01/SimpleProtoDemo.java
+++ src/main/java/org/amadeus/nxitech/example01/SimpleProtoDemo.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 public class SimpleProtoDemo {
     private static final Logger log = LoggerFactory.getLogger(SimpleProtoDemo.class);
     public static void main(String[] args) {
-        var person = Person.newBuilder().setAge(38).setName("abc").build();
-        log.info("{}", person);
+       // var person = Person.newBuilder().setAge(38).setName("abc").build();
+       // log.info("{}", person);
     }
 }
